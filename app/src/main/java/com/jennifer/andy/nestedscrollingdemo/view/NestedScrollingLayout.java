78d8aab4b778c552f79cb55fded24ffbbb515972
AndyJennifer <1225868370@qq.com>
2018-08-08 20:21:52 +0800

    修改了快速滑动导致NestedScrolling滑动的问题

--- app/src/main/java/com/jennifer/andy/nestedscrollingdemo/view/NestedScrollingLayout.java
+++ app/src/main/java/com/jennifer/andy/nestedscrollingdemo/view/NestedScrollingLayout.java
@@ -1,9 +1,11 @@
 package com.jennifer.andy.nestedscrollingdemo.view;
 
+import android.animation.ValueAnimator;
 import android.content.Context;
 import android.support.annotation.Nullable;
 import android.support.v4.view.ViewCompat;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
 import android.widget.LinearLayout;
 
@@ -19,7 +21,10 @@ public class NestedScrollingLayout extends LinearLayout {
 
     private View mHeadView;
     private int mHeadTopHeight;
+    private ValueAnimator mValueAnimator;
+
     public static final String TAG = "NestedScrollingLayout";
+    private static final int MAX_OFFSET_ANIMATION_DURATION = 600;
 
     public NestedScrollingLayout(Context context) {
         super(context);
@@ -71,18 +76,81 @@ public class NestedScrollingLayout extends LinearLayout {
     }
 
 
+    /**
+     * 当前父view执行快速滑动
+     *
+     * @param target    当前将要响应快速滑动的子view
+     * @param velocityX 横向速度 大于0向左滑
+     * @param velocityY 竖直速度 大于0向上滑
+     * @param consumed  当前子view是否能消费掉快速滑动，（因为有可能,当前子view以及滑动到头了）
+     * @return 返回true, 表示当前父view，执行了快速滑动
+     */
+    @Override
+    public boolean onNestedFling(View target, float velocityX, float velocityY, boolean consumed) {
+        Log.i(TAG, "onNestedFling: velocityY" + velocityY);
+        // 还要处理快速滑动的情况 可能快速滑动，导致headView没有完全显示，或者没有完全隐藏
+        if (velocityY > 0 && getScrollY() > 0 && getScrollY() < mHeadTopHeight) {//向上滑
+            startAnimation(200, getScrollY(), mHeadTopHeight);
+        } else {
+            startAnimation(300, getScrollY(), 0);
+        }
+        return true;
+    }
+
+
+    /**
+     * 在内部子view快速滑动之前，当前父View是否消耗
+     *
+     * @param target    当前将要响应快速滑动的子view
+     * @param velocityX 横向速度 大于0向左滑
+     * @param velocityY 竖直速度 大于0向上滑
+     * @return 返回false 则父view不消耗，返回true则消耗，那么子View就不能进行快速滑动
+     */
+    @Override
+    public boolean onNestedPreFling(View target, float velocityX, float velocityY) {
+        Log.i(TAG, "onNestedPreFling:getScrollY--> " + getScrollY());
+        return false;
+    }
+
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
         //计算headView高度
         measureChild(mHeadView, widthMeasureSpec, heightMeasureSpec);
         mHeadTopHeight = mHeadView.getHeight();
+
+        //重新计算高度，不然隐藏了headView后下面会空一截headView的高度
+        int height = MeasureSpec.getSize(heightMeasureSpec);
+        int heightMode = MeasureSpec.getMode(heightMeasureSpec);
+        int newMeasureSpec = MeasureSpec.makeMeasureSpec(height + mHeadTopHeight, heightMode);
+        super.onMeasure(widthMeasureSpec, newMeasureSpec);
+
     }
+
 
     @Override
     protected void onFinishInflate() {
         super.onFinishInflate();
         mHeadView = findViewById(R.id.iv_head_image);
+    }
+
+
+    private void startAnimation(long duration, int startY, int endY) {
+        if (mValueAnimator == null) {
+            mValueAnimator = new ValueAnimator();
+            mValueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                @Override
+                public void onAnimationUpdate(ValueAnimator animation) {
+                    int animatedValue = (int) animation.getAnimatedValue();
+                    scrollTo(0, animatedValue);
+                }
+            });
+        } else {
+            mValueAnimator.cancel();
+        }
+        mValueAnimator.setIntValues(startY, endY);
+        mValueAnimator.setDuration(duration);
+        mValueAnimator.start();
     }
 
 
