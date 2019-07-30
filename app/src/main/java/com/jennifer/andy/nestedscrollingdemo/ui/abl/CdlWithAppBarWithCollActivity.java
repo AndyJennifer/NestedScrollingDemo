e07411304a9ad48157a95fabe3b2baa7cf546a5d
AndyJennifer <1225868370@qq.com>
2019-07-30 22:06:03 +0800

    添加了CDL的demo

--- app/src/main/java/com/jennifer/andy/nestedscrollingdemo/ui/abl/CdlWithAppBarWithCollActivity.java
+++ app/src/main/java/com/jennifer/andy/nestedscrollingdemo/ui/abl/CdlWithAppBarWithCollActivity.java
@@ -2,8 +2,15 @@ package com.jennifer.andy.nestedscrollingdemo.ui.abl;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 import com.jennifer.andy.nestedscrollingdemo.R;
+import com.jennifer.andy.nestedscrollingdemo.adapter.SimpleStringAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Author:  andy.xwt
@@ -16,7 +23,22 @@ public class CdlWithAppBarWithCollActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_cdl_abl_ctl);
+        initView();
     }
 
+    private void initView() {
+        final RecyclerView recyclerView = findViewById(R.id.recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(new SimpleStringAdapter(initStrings(), this));
+        recyclerView.addItemDecoration(new DividerItemDecoration(this, DividerItemDecoration.VERTICAL));
+    }
+
+    private List<String> initStrings() {
+        List<String> list = new ArrayList<>();
+        for (int i = 0; i < 100; i++) {
+            list.add(i + "--->CollapsingToolbarLayout与AppBarLayout");
+        }
+        return list;
+    }
 }
